77722366322f7f0724cb5503c4d206331990f888
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-07-15 22:18:34 -0700

    updated web.js and added text to index.html

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var buffer = fs.readFileSync("index.html");
 
 app.get('/', function(request, response){
-  response.send('Hello World');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
-  console.log("Listening on" + port);
+  console.log("Listening on " + port);
 });
